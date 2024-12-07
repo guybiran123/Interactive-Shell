1ab2a822ae3dd6f2be0ad1a63f27ed79223f1c30
Guy Biran <guybiran123@gmail.com>
2024-12-07 19:12:06 +0200

    Adding Message_s struct

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,10 @@
 #include "utils.h"
 
+void Message_s::initialize() {
+    doesExist = false;
+    message = "";
+}
+
 std::vector<std::string> split(const std::string& str, char delimiter) {
     std::vector<std::string> tokens; // Stores the result (split substrings)
     std::stringstream ss(str);       // Wraps the input string into a stringstream object
--- utils.h
+++ utils.h
@@ -4,5 +4,12 @@
 #include <string>
 #include <sstream>
 
+struct Message_s {
+	bool doesExist{false};
+	std::string message{};
+
+	void initialize();
+};
+
 std::vector<std::string> split(const std::string& str, char delimiter);
 
